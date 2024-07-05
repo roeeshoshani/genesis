ac59eebc353a494aa852e7941cb0b7882f818ca7
Roee Shoshani <roysho10@gmail.com>
2024-07-06 00:50:39 +0300

    add a cargo xtask build command

--- kernel/core/src/main.rs
+++ kernel/core/src/main.rs
@@ -8,7 +8,21 @@ fn panic(_: &PanicInfo) -> ! {
     loop {}
 }
 
+fn foo() {}
+
+fn bar() {}
+
+#[inline(never)]
+fn shit() {
+    unsafe { BAR = foo };
+}
+
+static mut SHIT: fn() = foo;
+static mut BAR: fn() = bar;
+
 #[no_mangle]
 extern "C" fn _start() {
-    loop {}
+    loop {
+        shit();
+    }
 }
--- kernel/xtask/src/main.rs
+++ kernel/xtask/src/main.rs
@@ -1,3 +1,26 @@
+use anyhow::Result;
+use clap::Parser;
+use devx_cmd::{cmd, run, Cmd};
+
+#[derive(Parser)]
+enum Cli {
+    Build,
+}
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = run() {
+        eprintln!("error: {:#?}", err);
+    }
+}
+
+fn run() -> Result<()> {
+    let cli = Cli::parse();
+    match cli {
+        Cli::Build => build()?,
+    }
+    Ok(())
+}
+
+fn build() -> Result<()> {
+    cmd!("cargo", "build").current_dir("core").run()?;
+    Ok(())
 }
