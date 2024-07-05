fd11b08326347a26ae560eb25d541d65fdf1ca7c
Roee Shoshani <roysho10@gmail.com>
2024-07-05 22:10:25 +0300

    use a cargo workspace so that i can use cargo-xtask

--- kernel/build.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    println!("cargo::rerun-if-changed=kernel.lds");
-    println!("cargo::rustc-link-arg=-Tkernel.lds");
-}
---
+++ kernel/core/build.rs
@@ -0,0 +1,7 @@
+use std::env::current_dir;
+
+fn main() {
+    let linker_script = current_dir().unwrap().join("kernel.lds");
+    println!("cargo::rerun-if-changed={}", linker_script.display());
+    println!("cargo::rustc-link-arg=-T{}", linker_script.display());
+}
