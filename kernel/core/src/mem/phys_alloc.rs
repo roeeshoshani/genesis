d22de14cd07b2ba1dc0cfc45cbf84571c7b0c281
Roee Shoshani <roysho10@gmail.com>
2025-01-26 01:55:54 +0200

    implement deallocation in the physical memory allocator

--- kernel/core/src/mem/phys_alloc.rs
+++ kernel/core/src/mem/phys_alloc.rs
@@ -2,10 +2,7 @@ use core::{alloc::Layout, ptr::NonNull};
 
 use hal::mem::PhysAddr;
 
-use crate::{
-    mem::{is_aligned, page_alloc::alloc_pages},
-    println,
-};
+use crate::mem::{is_aligned, page_alloc::alloc_pages};
 
 use super::{
     align_up, align_up_p2,
@@ -26,6 +23,10 @@ type ChunkCursorSnapshot = AllocatorHdrCursorSnapshot<Chunk>;
 fn get_chunk_phys_addr(hdr: &ChunkHdr) -> PhysAddr {
     // we use the kseg cachable memory region for our virtual addressing
     hdr.virt_addr().kseg_cachable_phys_addr().unwrap()
+}
+
+fn get_chunk_phys_end_addr(hdr: &ChunkHdr) -> PhysAddr {
+    get_chunk_phys_addr(hdr) + hdr.data.size
 }
 
 /// the maximum allowed size of an allocation, in pages
@@ -57,11 +58,11 @@ impl PhysAllocator {
             // align the chunk's address to the desired alignment
             let aligned_addr = unsafe {
                 // SAFETY: the alignment value of a layout object must be a power of 2.
-                align_up_p2(chunk_phys_addr.0, layout.align())
+                PhysAddr(align_up_p2(chunk_phys_addr.0, layout.align()))
             };
 
             // calculate the amount of padding required from the start of the chunk for the allocation to be properly aligned.
-            let alignment_padding = aligned_addr - chunk_phys_addr.0;
+            let alignment_padding = aligned_addr.0 - chunk_phys_addr.0;
 
             // make sure that the chunk is big enough to satisfy this allocation.
             // take into account the padding created due to alignment.
@@ -125,12 +126,12 @@ impl PhysAllocator {
         // align up the address of the chunk to get the allocated address
         let allocated_addr = unsafe {
             // SAFETY: the alignment value of a layout object must be a power of 2.
-            align_up_p2(chunk_phys_addr.0, layout.align())
+            PhysAddr(align_up_p2(chunk_phys_addr.0, layout.align()))
         };
 
         // check if we have any padding at the start of the chunk.
         // if so, then we need to keep the chunk header, and sets its size to the size of the padding.
-        let padding = allocated_addr - chunk_phys_addr.0;
+        let padding = allocated_addr.0 - chunk_phys_addr.0;
         if padding != 0 {
             // make sure that the padding is large enough to hold a chunk header.
             //
@@ -145,6 +146,9 @@ impl PhysAllocator {
 
             // set the size of the chunk to the size of the padding, so that it covers the entire padding area.
             chosen_chunk.hdr.data.size = padding;
+        } else {
+            // no padding at the start of the chunk. remove this chunk, since this memory is now allocated.
+            chosen_chunk.unlink();
         }
 
         // calculate the size of the postfix. the postfix is the part of memory that is left at the end of the chunk after allocating.
@@ -164,7 +168,7 @@ impl PhysAllocator {
             debug_assert!(postfix_size >= size_of::<ChunkHdr>());
 
             // calculate the address of the postfix.
-            let postfix_addr = chosen_chunk.hdr.virt_addr() + padding + layout.size();
+            let postfix_addr = allocated_addr + layout.size();
 
             // make sure that the address of the postfix is aligned to the size of a chunk header.
             //
@@ -176,13 +180,6 @@ impl PhysAllocator {
             // so, this address must also be aligned to it.
             debug_assert!(is_aligned(postfix_addr.0, size_of::<ChunkHdr>()));
 
-            // create a pointer to a chunk header which we will put at the postfix.
-            let postfix_chunk_ptr = unsafe {
-                // all of our virtual addresses are in the kseg cachable memory region, and addresses in that region can never
-                // be null.
-                NonNull::new_unchecked(postfix_addr.as_mut_ptr::<ChunkHdr>())
-            };
-
             unsafe {
                 // SAFETY:
                 // as previously explained, the postfix padding is large enough to hold a chunk header, and the postfix
@@ -190,12 +187,14 @@ impl PhysAllocator {
                 //
                 // additionally, this memory is no longer used since we shrinked the original chunk to only cover the padding
                 // at the start of the chunk.
-                self.freelist
-                    .init_and_push_front(postfix_chunk_ptr, Chunk { size: postfix_size })
+                //
+                // moreover, this memory was part of an existing chunk, so it must be accessible through the kseg cachable memory
+                // segment.
+                self.add_free_chunk(postfix_addr, postfix_size);
             };
         }
 
-        Some(PhysAddr(allocated_addr))
+        Some(allocated_addr)
     }
 
     /// fix the layout of an allocation request so that it matches the layout expectations of this allocator.
@@ -234,7 +233,11 @@ impl PhysAllocator {
         Layout::from_size_align(size, align).unwrap()
     }
 
-    pub fn allocate(&mut self, layout: Layout) -> Option<PhysAddr> {
+    /// allocates physical memory according to the given layout.
+    ///
+    /// # safety
+    /// the memory must be deallocated once you finish using it.
+    pub unsafe fn allocate(&mut self, layout: Layout) -> Option<PhysAddr> {
         // fix the layout according to our layout requirements
         let layout = Self::fix_layout(layout);
 
@@ -270,11 +273,6 @@ impl PhysAllocator {
         };
 
         // create a new chunk to represent the region covered by those pages
-        let new_chunk_addr = raw_pages.addr.kseg_cachable_addr().unwrap();
-        let new_chunk_ptr = unsafe {
-            // SAFETY: kseg cachable addresses are never null
-            NonNull::new_unchecked(new_chunk_addr.as_mut_ptr::<ChunkHdr>())
-        };
         unsafe {
             // SAFETY:
             // the address is page aligned, which should be enough of an alignment to store our chunk header.
@@ -282,17 +280,88 @@ impl PhysAllocator {
             // additionally, the memory region is composed of multiple pages, which is large enough to store our chunk header.
             //
             // moreover, it is unused memory since we just allocated it from the page allocator.
-            self.freelist.init_and_push_front(
-                new_chunk_ptr,
-                Chunk {
-                    size: MAX_ALLOCATION_SIZE,
-                },
-            )
+            //
+            // furthermore, this region is accessible through the kseg cachable memory segment, since the page allocator only
+            // returns pages that are accessible through that region.
+            self.add_free_chunk(raw_pages.addr, MAX_ALLOCATION_SIZE);
         };
 
         // after adding that extra memory to the freelist, try once again to allocate from it, this time with no other fallback
         // option.
         self.allocate_from_freelist(layout)
+    }
+
+    /// adds a new free chunk to the freelist.
+    ///
+    /// # safety
+    /// - the address must be a valid chunk header pointer
+    /// - the memory must not be used, and must not be part of another chunk
+    /// - the memory must be accessible through the kseg cachable memory segment
+    unsafe fn add_free_chunk(&mut self, addr: PhysAddr, size: usize) {
+        let virt_addr = addr.kseg_cachable_addr().unwrap();
+        let hdr_ptr = unsafe {
+            // SAFETY: kseg cachable addresses are never null.
+            NonNull::new_unchecked(virt_addr.as_mut_ptr::<ChunkHdr>())
+        };
+        self.freelist.init_and_push_front(hdr_ptr, Chunk { size });
+    }
+
+    pub fn dealloc(&mut self, addr: PhysAddr, layout: Layout) {
+        // fix the layout according to our layout requirements
+        let layout = Self::fix_layout(layout);
+
+        // extrat the size of the layout.
+        // this is mutable since the size may be modified later as part of the deallocating algorithm.
+        let mut size = layout.size();
+
+        // calculate the end address of the deallocated region.
+        let end_addr = addr + size;
+
+        // handle the case where we have an adjacent free chunk right after the deallocated chunk
+        let mut cursor = self.freelist.cursor();
+        let maybe_chunk_after = cursor.find(|hdr| get_chunk_phys_addr(hdr) == end_addr);
+        if let Some(chunk_after) = maybe_chunk_after {
+            // in this case, what we want to do is:
+            // - remove the existing chunk that is after the deallocated chunk
+            // - increase the size of the deallocated chunk to include the existing chunk
+            //
+            // this way we can cover the entire combined region with a single chunk
+
+            // count its bytes as part of the deallocated chunk
+            size += chunk_after.hdr.data.size;
+
+            // remove it from the freelist
+            chunk_after.unlink();
+
+            // after doing this, we can continue with the regular deallocation flow, and the chunk that was after the
+            // deallocated chunk will now be counted as part of it.
+        }
+
+        // now, decide how to deallocate based on whether we have a free chunk before the deallocated chunk
+        let mut cursor = self.freelist.cursor();
+        let maybe_chunk_before = cursor.find(|hdr| get_chunk_phys_end_addr(hdr) == addr);
+        match maybe_chunk_before {
+            Some(chunk_before) => {
+                // we have a free chunk right before the deallocated chunk.
+                //
+                // so, to deallocate it, we can just increase the size of that chunk that comes before it to include the deallocated
+                // chunk in it.
+                chunk_before.hdr.data.size += size;
+            }
+            None => {
+                // no adjacent free chunk before the deallocated chunk, just add it as a new chunk in the freelist.
+                unsafe {
+                    // SAFETY: this address is a valid header pointer since it was previously returned from our allocation,
+                    // and we only return addresses that are valid header pointers.
+                    //
+                    // additionally, this address is no longer used because it is currently being deallocated.
+                    //
+                    // furthermore, this address is accessible through the kseg cachable memory region since it was previously
+                    // returned from our allocation.
+                    self.add_free_chunk(addr, size);
+                }
+            }
+        }
     }
 }
 
