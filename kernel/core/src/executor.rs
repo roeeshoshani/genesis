d774f3a1ae72cae6f2c7a1033a5f2a7c0ac4b26a
Roee Shoshani <roysho10@gmail.com>
2025-01-31 16:21:04 +0200

    remove all unsafe code from executor

--- kernel/core/src/executor.rs
+++ kernel/core/src/executor.rs
@@ -1,27 +1,27 @@
 use core::{
-    cell::UnsafeCell,
     future::Future,
-    mem::ManuallyDrop,
-    ops::Deref,
     pin::Pin,
-    ptr::null,
     sync::atomic::{AtomicBool, Ordering},
-    task::{Context, Poll, RawWaker, RawWakerVTable, Waker},
+    task::{Context, Poll, Waker},
 };
 
-use alloc::{boxed::Box, sync::Arc, vec::Vec};
+use alloc::{boxed::Box, sync::Arc, task::Wake, vec::Vec};
 
 use crate::sync::IrqSpinlock;
 
 pub struct Task {
     should_be_polled: AtomicBool,
-
-    // using an unsafe cell here since this part is only accessed by the executor, so it doesn't need synchronization.
-    future: UnsafeCell<Pin<Box<dyn Future<Output = ()> + 'static + Send>>>,
+    future: IrqSpinlock<Pin<Box<dyn Future<Output = ()> + 'static + Send>>>,
 }
+impl Wake for Task {
+    fn wake(self: Arc<Self>) {
+        self.should_be_polled.store(true, Ordering::Relaxed);
+    }
 
-// SAFETY: we only access the future from within the executor.
-unsafe impl Sync for Task {}
+    fn wake_by_ref(self: &Arc<Self>) {
+        self.should_be_polled.store(true, Ordering::Relaxed);
+    }
+}
 
 fn poll_task_retain(task: &mut Arc<Task>) -> bool {
     if !task.should_be_polled.swap(false, Ordering::Relaxed) {
@@ -29,19 +29,12 @@ fn poll_task_retain(task: &mut Arc<Task>) -> bool {
         return true;
     }
 
-    // get a refcount on the task for this waker
-    let task_clone = task.clone();
-    let waker = unsafe {
-        // SAFETY: todo...
-        Waker::new(Arc::into_raw(task_clone).cast(), &WAKER_VTABLE)
-    };
-
+    let waker = Waker::from(task.clone());
     let mut context = Context::from_waker(&waker);
-    let future = unsafe {
-        // SAFETY: we only access the future from within the executor.
-        (*task.future.get()).as_mut()
-    };
-    let poll_res = Future::poll(future, &mut context);
+
+    let mut future = task.future.lock();
+
+    let poll_res = Future::poll(future.as_mut(), &mut context);
     match poll_res {
         Poll::Ready(()) => {
             // task has finished executing. don't keep it in the list
@@ -69,7 +62,7 @@ impl Executor {
     {
         self.tasks.push(Arc::new(Task {
             should_be_polled: AtomicBool::new(true),
-            future: UnsafeCell::new(Box::pin(task)),
+            future: IrqSpinlock::new(Box::pin(task)),
         }));
     }
 
@@ -83,27 +76,3 @@ impl Executor {
 }
 
 pub static EXECUTOR: IrqSpinlock<Executor> = IrqSpinlock::new(Executor::new());
-
-unsafe fn vtable_clone(data: *const ()) -> RawWaker {
-    let task = ManuallyDrop::new(Arc::from_raw(data as *const Task));
-    let cloned_task = task.deref().clone();
-
-    RawWaker::new(Arc::into_raw(cloned_task).cast(), &WAKER_VTABLE)
-}
-
-unsafe fn vtable_wake(data: *const ()) {
-    let task = Arc::from_raw(data as *const Task);
-    task.should_be_polled.store(true, Ordering::Relaxed);
-}
-
-unsafe fn vtable_wake_by_ref(data: *const ()) {
-    let task = ManuallyDrop::new(Arc::from_raw(data as *const Task));
-    task.deref().should_be_polled.store(true, Ordering::Relaxed);
-}
-
-unsafe fn vtable_drop(data: *const ()) {
-    drop(Arc::from_raw(data as *const Task));
-}
-
-static WAKER_VTABLE: RawWakerVTable =
-    RawWakerVTable::new(vtable_clone, vtable_wake, vtable_wake_by_ref, vtable_drop);
